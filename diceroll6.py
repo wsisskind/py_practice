e2de2091f398dbd3439a37522b45f568db0f9c16
Josh Sisskind <6191659+sisskind@users.noreply.github.com>
2020-07-23 22:18:25 -0400

    Added min value

--- diceroll6.py
+++ diceroll6.py
@@ -1,11 +1,24 @@
+import random
+
 def diceroll6():
 	dice = []
 	print('How many dice?')
 	n = input()
 	n = int(n)
+	p = round(n*.2)
 	for i in range(1, n+1):
 		d = random.randrange(1,7)
 		dice.append(d)
 		print('Dice', i, "-", d)
+	dice = removeMin(dice, p)
 	total = sum(dice)
 	print(total)
+
+def removeMin(l,n):
+	for j in range(0,n):
+		minval = min(l)
+		print ("Removing lowest value: " + str(minval))
+		l.remove(minval)
+	return l
+
+diceroll6()
